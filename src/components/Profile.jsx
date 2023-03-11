7fad01f244a8038d8977de56a7aa1a55ac5722e3
DongheeL <donghlee19@gmail.com>
2023-03-11 23:19:54 +0900

    컴포넌트에 props 대신 구조분해 문법을 사용한 props의 key를 명시.

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function Profile(props) {
+export default function Profile({image, name, title}) { //props 대신 props 안의 속성값을 명시해 주면 JSX에서 일일이 props.image, props.name, props.title가 아닌 image, name, title로 사용할 수 있음. 
     return (
         <div className='profile'>
-            <img className='photo' src={props.image} alt='avatar'></img>
-            <h1>{props.name}</h1>
-            <p>{props.title}</p>
+            <img className='photo' src={image} alt='avatar'></img>
+            <h1>{name}</h1>
+            <p>{title}</p>
         </div>
     );
 }
