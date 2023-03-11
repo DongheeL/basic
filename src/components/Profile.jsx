81520ffa04f34553d029a67aad7f2749bb4478e7
DongheeL <donghlee19@gmail.com>
2023-03-11 23:44:26 +0900

    isNew 수정

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function Profile({image, name, title, isNewMember }) { //props 대신 props 안의 속성값을 명시해 주면 JSX에서 일일이 props.image, props.name, props.title가 아닌 image, name, title로 사용할 수 있음. 
+export default function Profile({image, name, title, isNew }) { //props 대신 props 안의 속성값을 명시해 주면 JSX에서 일일이 props.image, props.name, props.title가 아닌 image, name, title로 사용할 수 있음. 
     return (
         <div className='profile'>
-            <div className='new'>{isNewMember}</div>
             <img className='photo' src={image} alt='avatar'></img>
+            {isNew && <span className='new'>NEW</span>}
             <h1>{name}</h1>
             <p>{title}</p>
         </div>
