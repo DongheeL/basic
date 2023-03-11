f9d8e9a5cd5cebba5dc0fd3bdff60f5f7fe5c155
DongheeL <donghlee19@gmail.com>
2023-03-11 23:15:30 +0900

    props 사용

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function Profile() {
+export default function Profile(props) {
     return (
         <div className='profile'>
-            <img className='photo' src='https://images.unsplash.com/photo-1676824438505-cb0927790b7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80' alt='avatar'></img>
-            <h1>James Kim</h1>
-            <p>프론트엔드 개발자</p>
+            <img className='photo' src={props.image} alt='avatar'></img>
+            <h1>{props.name}</h1>
+            <p>{props.title}</p>
         </div>
     );
 }
