99265dd7109507b17086e71fc5e97272cafe9292
DongheeL <donghlee19@gmail.com>
2023-03-12 22:16:34 +0900

    배열 상태관리 (멘토바꾸기)

---
+++ src/AppMentors.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+export default function AppMentor() {
+  const [person, setPerson] = useState({
+    name: '엘리',
+    title: '개발자',
+    mentors: [
+      {
+        name: '밥',
+        title: '시니어개발자',
+      },
+      {
+        name: '제임스',
+        title: '시니어개발자',
+      },
+    ],
+  });
+  return (
+    <div>
+      <h1>
+        {person.name}는 {person.title}
+      </h1>
+      <p>{person.name}의 멘토는:</p>
+      <ul>
+        {person.mentors.map((mentor, index) => (
+          <li key={index}>
+            {mentor.name} ({mentor.title})
+          </li>
+        ))}
+      </ul>
+      <button
+        onClick={() => {
+          const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
+          const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
+          const newMentor = person.mentors.map(val=> val.name==prev ? val = {...val,name:current} : val);
+          setPerson(person=>({...person, mentors:newMentor}))
+        }}
+      >
+        멘토의 이름을 바꾸기
+      </button>
+    </div>
+  );
+}
