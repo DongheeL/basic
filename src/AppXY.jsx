1e48d8ccca3cab407cfd617452f608ee9090db34
DongheeL <donghlee19@gmail.com>
2023-03-12 21:33:29 +0900

    마우스 따라가기 - 수정

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -2,16 +2,15 @@ import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
-  const [x,setX] = useState(-15);
-  const [y,setY] = useState(-15);
-  const handleChange = (event)=>{
-    setX(event.clientX);
-    setY(event.clientY);
-  }
+  const [x,setX] = useState(0);
+  const [y,setY] = useState(0);
 
   return (
-    <div className='container' onMouseMove={handleChange}>
-      <div className='pointer' style={{left:`${x}px`,top:`${y}px`}}/>
+    <div className='container' onPointerMove={(e)=>{
+      setX(e.clientX);
+      setY(e.clientY);
+    }}>
+      <div className='pointer' style={{transform:`translate(${x}px, ${y}px)`}}/>
     </div>
   );
 }
