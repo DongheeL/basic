3efdb377f46bd90f0352a05d5473393e5aea6b80
DongheeL <donghlee19@gmail.com>
2023-03-15 23:14:47 +0900

    로딩, 에러 상태 추가

--- src/basic/AppProducts.jsx
+++ src/basic/AppProducts.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { Suspense, useState } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import './App.css';
-import Products from './components/Products';
+// import Products from './components/Products';
 
+const Products = React.lazy(()=> import('./components/Products'))
 export default function AppProducts() {
   const [showProducts, setShowProducts] = useState(true);
   return (
     <div>
-      {showProducts && <Products />}
-      <button onClick={() => setShowProducts((show) => !show)}>Toggle</button>
+      <ErrorBoundary fallback={<div>에러가 발생했습니다.</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+          {showProducts && <Products />}
+        </Suspense>
+      </ErrorBoundary>
+        <button onClick={() => setShowProducts((show) => !show)}>Toggle</button>
     </div>
   );
 }
